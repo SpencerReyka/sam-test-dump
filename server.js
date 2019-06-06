e8d0410f90ea8f4914bb4714b3c3b38bb6eb663f
Spencer Reyka <spencer.reyka@gmail.com>
2019-06-05 20:52:28 -0500

    small fix

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 // server.js
-
 const express = require('express');
 const app = express();
-
-// Set the port
 const PORT = process.env.PORT || 3000;
+// server.js
 
-// Import Path
-const path = require('path');
 
-// Static files
-app.use(express.static('build'));
 
 // API Routes
 app.use('/api/quotes', require('../routes/quote-routes'));
 
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
 app.listen(PORT, () => {
   console.log(`App is up and running. Listening on port ${PORT}`);
 });
+
+\
